5584a91d6fe70f4e53fd9234252b3bba4bc01e51
Edison Wang <ew040607@gmail.com>
2025-10-04 01:59:27 -0400

    created and added serializers for models

---
+++ hivemind/notebooks/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import User, Notebook, Page, Version
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class NotebookSerializer(serializers.ModelSerializer):
+    admin_id = UserSerializer(read_only=True)
+    user_ids = UserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Notebook
+        fields = ['notebook_id', 'admin_id', 'title', 'user_ids', 'created_at', 'updated_at']
+    
+class VersionSerializer(serializers.ModelSerializer):
+    user_id = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Version
+        fields = ['version_id', 'user_id', 'previous_version', 'content', 'created_at']
+
+class PageSerializer(serializers.ModelSerializer):
+    notebook = NotebookSerializer(read_only=True)
+    latest_version = VersionSerializer(read_only=True)
+
+    class Meta:
+        model = Page
+        fields = ['page_id', 'notebook', 'title', 'latest_version', 'created_at', 'updated_at']
