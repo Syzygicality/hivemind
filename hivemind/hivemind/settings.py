aa6ffbf0dba72d5f88529d1a3d01c57002e67e0a
Edison Wang <ew040607@gmail.com>
2025-10-04 17:28:55 -0400

    defined auth and permission classes in settings

--- hivemind/hivemind/settings.py
+++ hivemind/hivemind/settings.py
@@ -74,6 +74,15 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 WSGI_APPLICATION = 'hivemind.wsgi.application'
 
 
