6c13e72c6e239c812fc8e799ec8ca994cb7a271b
Edison Wang <ew040607@gmail.com>
2025-10-04 14:19:38 -0400

    created draft and post serializers

--- hivemind/notebooks/serializers.py
+++ hivemind/notebooks/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Notebook, Page, Version
+from .models import User, Notebook, Page, Version, Draft, Post
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,9 +22,26 @@ class VersionSerializer(serializers.ModelSerializer):
         fields = ['version_id', 'user_id', 'page_id', 'previous_version', 'content', 'created_at']
 
 class PageSerializer(serializers.ModelSerializer):
-    notebook = NotebookSerializer(read_only=True)
+    notebook_id = NotebookSerializer(read_only=True)
     latest_version = VersionSerializer(read_only=True)
 
     class Meta:
         model = Page
         fields = ['page_id', 'notebook_id', 'title', 'latest_version', 'created_at', 'updated_at']
+
+class DraftSerializer(serializers.ModelSerializer):
+    user_id = UserSerializer(read_only=True)
+    page_id = PageSerializer(read_only=True)
+
+    class Meta:
+        model = Draft
+        fields = ['draft_id', 'user_id', 'page_id', 'content', 'created_at', 'updated_at']
+
+class PostSerializer(serializers.ModelSerializer):
+    user_id = UserSerializer(read_only=True)
+    page_id = PageSerializer(read_only=True)
+    draft_id = DraftSerializer(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['post_id', 'user_id', 'page_id', 'draft_id', 'votes', 'created_at', 'updated_at']
