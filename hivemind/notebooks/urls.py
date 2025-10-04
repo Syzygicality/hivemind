640d16a424cd6da3edc72970a8499b77d71e9fe4
Edison Wang <ew040607@gmail.com>
2025-10-04 10:17:30 -0400

    setup page GCBVs and views

--- hivemind/notebooks/urls.py
+++ hivemind/notebooks/urls.py
@@ -1,4 +1,4 @@
-from .views import UserListCreateView, UserDetailView, NotebookListCreateView, NotebookDetailView, PageViewSet, VersionViewSet
+from .views import UserListCreateView, UserDetailView, NotebookListCreateView, NotebookDetailView, PageListCreateView, PageDetailView
 from django.urls import path
 
 urlpatterns = [
@@ -6,4 +6,6 @@ urlpatterns = [
     path('users/<uuid:id>/', UserDetailView.as_view(), name='user-detail'),
     path('notebooks/', NotebookListCreateView.as_view(), name='notebook-list-create'),
     path('notebooks/<uuid:notebook_id>/', NotebookDetailView.as_view(), name='notebook-detail'),
+    path('pages/', PageListCreateView.as_view(), name='page-list-create'),
+    path('pages/<uuid:page_id>', PageDetailView.as_view(), name='page-detail'),
 ]
--- hivemind/notebooks/views.py
+++ hivemind/notebooks/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .models import User, Notebook, Page, Version
 from .serializers import UserSerializer, NotebookSerializer, PageSerializer, VersionSerializer
 from rest_framework import permissions
+from django.db import transaction
 
 # Create your views here.
 class UserListCreateView(generics.ListCreateAPIView):
@@ -19,7 +20,7 @@ class NotebookListCreateView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     lookup_field = 'notebook_id'
 
-    def get_queryset(self):
+    def get_queryset(self, request, *args, **kwargs):
         user = self.request.user
         return (
             Notebook.objects.filter(admin_id=user) |
@@ -31,9 +32,35 @@ class NotebookDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
     lookup_field = 'notebook_id'
 
-    def get_queryset(self):
+    def get_queryset(self, request, *args, **kwargs):
         user = self.request.user
         return (
             Notebook.objects.filter(admin_id=user) |
             Notebook.objects.filter(user_ids=user)
         ).distinct().order_by('-updated_at')
+
+class PageListCreateView(generics.ListCreateAPIView):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+    lookup_field = 'page_id'
+
+    def get_queryset(self, request, *args, **kwargs):
+        notebook_id = kwargs.get('notebook_id')
+        return Page.objects.filter(notebook_id=notebook_id).order_by('-created_at')
+    
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            page = serializer.save(notebook_id=self.request.data.get('notebook_id'))
+            version = Version.objects.create(
+                user_id=self.request.user,
+                previous_version=None,
+                content=""
+            )
+            page.latest_version = version
+            page.save()
+        return version
+
+class PageDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+    lookup_field = 'page_id'
